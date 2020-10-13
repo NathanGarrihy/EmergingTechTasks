f28e9268c778a74cb40b992afd97c36f680bdd45
NathanGarrihy <37175360+NathanGarrihy@users.noreply.github.com>
2020-10-13 16:11:04 +0100

    Updated algorithm, now prints to 52 decimal places, working on 100

--- sqrt2.py
+++ sqrt2.py
@@ -1,15 +1,34 @@
-def sqrt(x):
+SQRTNUM = 2
+def guessSqrt2():
     """
-    A function to calculate the square root of a number x.
+    A function to calculate the approx square root of a number x.
     """
     # Initial guess for the square root (z)
-    z = x / 2
+    z = SQRTNUM * .5
     # Loop until accuracy is sufficiant
-    while abs(x - (z * z)) > 0.0000001:
+    while abs(SQRTNUM - (z * z)) > 0.000000000000001: # maximum accuracy
         # Calculate a better guess for square root
-        z -= (z*z - x) / (2*z)
+        z -= (z*z - SQRTNUM) / (SQRTNUM*z)
     # Return the approx square root of x
-    return z
-
+    # 52 decimal places is max for now
+    return "{0:.100f}".format(z)
+    
+def sqrt2():
+    """
+    A function that returns the accurate square root of a number to 100 decimal places
+    """
+    r = SQRTNUM
+    x = SQRTNUM
+    
+    precision = 10 ** (-10)
+    
+    while abs(x - r * r) > precision:
+        r = (r + x / r) / 2
+        
+    return "{0:.52f}".format(r)
+    
+    
+    
 # Print square root of 2
-print(sqrt(2))
+print(guessSqrt2())
+print(sqrt2())
