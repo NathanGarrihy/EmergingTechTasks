416b1d190d105cdee89f2143285d66614f567527
NathanGarrihy <37175360+NathanGarrihy@users.noreply.github.com>
2020-10-13 16:12:37 +0100

    re-commiting changes due to last commit failure

--- sqrt2.py
+++ sqrt2.py
@@ -1,5 +1,5 @@
 SQRTNUM = 2
-def guessSqrt2():
+def Sqrt2():
     """
     A function to calculate the approx square root of a number x.
     """
@@ -13,22 +13,6 @@ def guessSqrt2():
     # 52 decimal places is max for now
     return "{0:.100f}".format(z)
     
-def sqrt2():
-    """
-    A function that returns the accurate square root of a number to 100 decimal places
-    """
-    r = SQRTNUM
-    x = SQRTNUM
-    
-    precision = 10 ** (-10)
-    
-    while abs(x - r * r) > precision:
-        r = (r + x / r) / 2
-        
-    return "{0:.52f}".format(r)
-    
-    
-    
 # Print square root of 2
-print(guessSqrt2())
-print(sqrt2())
+print(Sqrt2())
+
