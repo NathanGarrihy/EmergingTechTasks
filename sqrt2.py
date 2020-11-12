d1e5ef0ce79b508ddd642129948166dfc0edf4ec
NathanGarrihy <37175360+NathanGarrihy@users.noreply.github.com>
2020-11-12 00:02:03 +0000

    Completed sqrt2 task

--- sqrt2.py
+++ sqrt2.py
@@ -1,18 +1,20 @@
-SQRTNUM = 2
 def Sqrt2():
-    """
-    A function to calculate the approx square root of a number x.
-    """
-    # Initial guess for the square root (z)
-    z = SQRTNUM * .5
-    # Loop until accuracy is sufficiant
-    while abs(SQRTNUM - (z * z)) > 0.000000000000001: # maximum accuracy
-        # Calculate a better guess for square root
-        z -= (z*z - SQRTNUM) / (SQRTNUM*z)
-    # Return the approx square root of x
-    # 52 decimal places is max for now
-    return "{0:.100f}".format(z)
+# Move numbers from left to right
+    a = 2 *(10**(200))
+    b = 0
+    # set c to a googol(10^100)
+    # googol is 10 to the power of 100 (100 decimal places)
+    googol = 1 *(10**100)
     
-# Print square root of 2
-print(Sqrt2())
+    # Loop until accuracy is sufficiant
+    while b != googol:
+        # Set b to googol
+        b = googol
+        # Get the floor division + Bit shift num 1 place to the right
+        googol = (b+(a//b)) >> 1
+        # Keeps running until b == googol
+    # Returns numbers to the right of decimal place
+    return googol-(10**100)
 
+# Print square root of 2
+print("1.%d"%Sqrt2())
