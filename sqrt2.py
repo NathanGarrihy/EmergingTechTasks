9841a4306311484b5188504871a8f9b977f7bf91
NathanGarrihy <37175360+NathanGarrihy@users.noreply.github.com>
2020-11-12 22:00:42 +0000

    Finalized program

--- sqrt2.py
+++ sqrt2.py
@@ -1,20 +1,24 @@
 def Sqrt2():
-# Move numbers from left to right
-    a = 2 *(10**(200))
+    # This constant is not fully required but can be changed
+    # in order to find the square root of a different number
+    SQRTNUM = 2;
+    # googol = 10 to the power of 100
+    # a = google value of 2 * googol. This moves
+    # numbers to the left side of the decimal place
+    a = SQRTNUM *(10**(200))
     b = 0
-    # set c to a googol(10^100)
-    # googol is 10 to the power of 100 (100 decimal places)
-    googol = 1 *(10**100)
+    googol = 1
     
     # Loop until accuracy is sufficiant
-    while b != googol:
+    while googol != b:
         # Set b to googol
         b = googol
         # Get the floor division + Bit shift num 1 place to the right
-        googol = (b+(a//b)) >> 1
         # Keeps running until b == googol
+        googol = (b+(a//b)) >> 1
     # Returns numbers to the right of decimal place
-    return googol-(10**100)
-
+    finalVal = googol-(10**100)
+    return "1.%d"%finalVal
+    
 # Print square root of 2
-print("1.%d"%Sqrt2())
+print(Sqrt2())
